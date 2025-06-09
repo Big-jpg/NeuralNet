1601270fef804bb88aa926d380bdc57899b42564
MOHD-SHAMOON-04 <mohdshamoon04@gmail.com>
2025-06-09 18:16:53 +0530

    minor changes

--- iris/model.js
+++ iris/model.js
@@ -5,15 +5,15 @@ import { fileURLToPath } from 'url';
 
 const nn = new NeuralNetwork(
     [TRAINING_DATA.inputs[0].length, 3, 3, TRAINING_DATA.labels[0].length],
-    0.01,
+    0.03,
     10000);
 
-nn.trainSGD(TRAINING_DATA);
+// nn.trainSGD(TRAINING_DATA);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// await nn.loadWB(path.join(__dirname, 'model.json'));
-nn.dumpWB(path.join(__dirname, 'model.json'));
+await nn.loadWB(path.join(__dirname, 'model.json'));
+// nn.dumpWB(path.join(__dirname, 'model.json'));
 
 let correct = 0;
 let totalTest = TESTING_DATA.inputs.length;
@@ -22,7 +22,8 @@ for (let i = 0; i < totalTest; i++) {
     const inputArr = TESTING_DATA.inputs[i];
     const labelArr = TESTING_DATA.labels[i];
     const prediction = nn.predict(inputArr);
-    // console.log(`| INPUT: ${inputArr} | OUTPUT: ${prediction} | LABEL: ${labelArr} |`);
+    const error = prediction.map((p, i) => (Math.pow(p - labelArr[i], 2)).toFixed('3'));
+    console.log(`| INPUT: ${inputArr} | OUTPUT: ${prediction.map(x => x.toFixed('3'))} | LABEL: ${labelArr} | ERROR: ${error} |`);
     if (prediction.indexOf(Math.max(...prediction)) === labelArr.indexOf(Math.max(...labelArr)))
         correct++;
 }
